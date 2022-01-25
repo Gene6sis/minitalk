e53aac40318b71a6c80b9eb65db01e1904dda4d4
Adam BEN MCHICHI <adben-mc@e2r9p10.clusters.42paris.fr>
2022-01-26 00:13:34 +0100

    client

--- srcs/client.c
+++ srcs/client.c
@@ -6,38 +6,59 @@
 /*   By: adben-mc <adben-mc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/23 22:22:52 by adben-mc          #+#    #+#             */
-/*   Updated: 2022/01/25 05:03:40 by adben-mc         ###   ########.fr       */
+/*   Updated: 2022/01/26 00:13:16 by adben-mc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-void ft_sendbit(int pid, char *message)
+static void	ft_sendbit(int pid, char *message)
 {
-	char c;
-	int i;
-	int send;
-	int err;
+	size_t	i;
 
-	err = 0;
 	i = 0;
 	while (message[i])
 	{
-		c = 0;
-		send = c & 1 << (7 - i);
-		if (send && !err)
-			if (kill(pid, SIGUSR1) == -1)
-				err = 1;
-		if (!send && !err)
-			if (kill(pid, SIGUSR2) == -1)
-				err = 1;
+		if (message[i] == '1')
+			kill(pid, SIGUSR1);
+		else if (message[i] == '0')
+			kill(pid, SIGUSR2);
+		i++;
 	}
-
 }
 
-int ft_arg_error(char *nbr, char *str, int *pid, char **message)
+char	*ft_stob(char *str)
 {
-	int i;
+	int		i;
+	int		j;
+	char	*result;
+	int		c;
+
+	i = 0;
+	result = ft_calloc(ft_strlen(str) * 8 + 1, sizeof(char));
+	if (!result)
+		return (NULL);
+	while (str[i])
+	{
+		c = (int)str[i];
+		j = i * 8 + 7;
+		while (j >= (i * 8))
+		{
+			if (c % 2 == 1)
+				result[j] = '1';
+			else
+				result[j] = '0';
+			c /= 2;
+			j--;
+		}
+		i++;
+	}
+	return (result);
+}
+
+static int	ft_arg_error(char *nbr, char *str)
+{
+	int	i;
 
 	i = 0;
 	while (nbr[i])
@@ -46,21 +67,26 @@ int ft_arg_error(char *nbr, char *str, int *pid, char **message)
 			return (1);
 		i++;
 	}
-	*pid = ft_atoi(nbr);
-	*message = str;
 	return (0);
 }
-
 
 int	main(int argc, char **argv)
 {
 	int		pid;
 	char	*message;
-	
-	if (argc != 3 || ft_arg_error(argv[1], argv[2], &pid, &message))
+
+	if (argc != 3)
 	{
-		ft_putstr_fd("client: invalid arguments.\n", 2);
-		exit(EXIT_FAILURE);
+		ft_printf("client: invalid arguments.\n");
+		return (0);
 	}
-	ft_sendbit(pid, message);
+	if (ft_arg_error(argv[1], argv[2]))
+	{
+		ft_printf("client: invalid pid.\n");
+		return (0);
+	}
+	message = ft_stob(argv[2]);
+	ft_sendbit(ft_atoi(argv[1]), message);
+	free(message);
+	return (0);
 }
